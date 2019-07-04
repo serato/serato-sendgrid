ad38e6572cd57e33aa0636c3c0c03086e123ccfa
Jing Xu <jingcareers@gmail.com>
2019-07-04 16:39:16 +1200

    [WEB-2971] implement the functionalities of sending emails using new json specification

--- src/Mailer.php
+++ src/Mailer.php
@@ -3,8 +3,14 @@ declare(strict_types=1);
 
 namespace Serato\SendGrid;
 
+use phpDocumentor\Reflection\Types\Boolean;
 use SendGrid;
 use SendGrid\Mail\Mail;
+use SendGrid\Mail\Category;
+use SendGrid\Mail\Cc;
+use SendGrid\Mail\Bcc;
+use SendGrid\Response;
+use Exception;
 
 class Mailer extends SendGrid
 {
@@ -24,35 +30,128 @@ class Mailer extends SendGrid
      *                        "impersonateSubuser" are implemented.
      * @param boolean $disableEmailDelivery
      */
-    public function __construct($apiKey, $options = array(), $disableEmailDelivery = false)
+    public function __construct($apiKey, $disableEmailDelivery = false, $options = array())
     {
         $this->disableEmailDelivery = $disableEmailDelivery;
         parent::__construct($apiKey, $options);
     }
 
     /**
-     * Overriding the send method to stop email delivery in load testing
+     * Fetch email information from email_config.json file by template name
      *
-     * @param \SendGrid\Mail\Mail $email A Mail object, containing the request object
-     *
-     * @return \SendGrid\Response | null
+     * @param string $templateName
+     * @return array
      */
-    public function send(Mail $email)
+
+    public function fetchEmailOptionsByTemplateName(String $templateName): array
     {
-        if ($this->disableEmailDelivery === true) {
-            return null;
+        // Read JSON file
+        $templatesJson = file_get_contents(__DIR__ . '/spec/email_config.json');
+        //Decode JSON
+        $templatesJsonData = '';
+        if (is_string($templatesJson)) {
+            $templatesJsonData = json_decode($templatesJson, true);
         }
-        return parent::send($email);
+        if ($templatesJsonData === null) {
+            // $templatesJsonData is null because the json cannot be decoded
+            throw new Exception('Invalid JSON file.');
+        }
+            //Traverse array and get the data for students aged less than 20
+        foreach ($templatesJsonData as $key => $value) {
+            if ($key == $templateName) {
+                return $value;
+            }
+        }
+        throw new Exception('Invalid template name');
     }
 
     /**
-     * Returns a configured Mail object
-     * @return Mail
+     * Check template params are valid
+     *
+     * @param array $templateParams
+     * @param array $emailOptTemplateParams
+     * @return array
      */
-    private function getMail()
+    public function validateTemplateParams(Array $templateParams, Array $emailOptTemplateParams): array
     {
+        $validTemplateParams = array();
+        foreach ($templateParams as $key => $value) {
+            if (in_array($key, $emailOptTemplateParams)) {
+                $validTemplateParams[$key] = $value;
+            }
+        }
+        return $validTemplateParams;
+    }
+
+    /**
+     * Check template language is valid
+     *
+     * @param string $language
+     * @param array $emailLanguageOptions
+     * @return bool
+     */
+    public function validateEmailLanguage(String $language, Array $emailLanguageOptions): bool
+    {
+        if (isset($emailLanguageOptions[$language]) && isset($emailLanguageOptions[$language]['template_id'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Send email
+     *
+     * @param string $templateName
+     * @return \SendGrid\Response | null
+     */
+    public function sendEmail(
+        string $templateName,
+        string $recipientName,
+        string $recipientEmail,
+        string $language,
+        array $templateParams
+    ):? Response {
+        $emailOptions = $this->fetchEmailOptionsByTemplateName($templateName);
+        $validTemplateParams = $this->validateTemplateParams($templateParams, $emailOptions['template_params']);
+
+        $emailLanguage = $this->validateEmailLanguage($language, $emailOptions['languages']) ? $language : 'en';
+        $templateId = $emailOptions['languages'][$emailLanguage]['template_id'];
+        $categories = array_merge(
+            $emailOptions['languages'][$emailLanguage]['categories'],
+            $emailOptions['categories']
+        );
+
+        // get a configured Mail object
         $mail = new Mail();
         $mail->setFrom(self::FROM_EMAIL, self::FROM_NAME);
-        return $mail;
+        foreach ($validTemplateParams as $key => $value) {
+              $mail->addSubstitution($key, $value);
+        }
+        foreach ($categories as $category) {
+            $mail->addCategory(new Category($category));
+        }
+        $mail->addTo($recipientEmail, $recipientName);
+        $mail->setTemplateId($templateId);
+        if (isset($emailOptions['recipients']) && isset($emailOptions['recipients']['bcc'])) {
+            foreach ($emailOptions['recipients']['bcc'] as $bcc) {
+                $mail->addBcc(new Bcc($bcc));
+            }
+        }
+        if (isset($emailOptions['recipients']) && isset($emailOptions['recipients']['cc'])) {
+            foreach ($emailOptions['recipients']['cc'] as $cc) {
+                $mail->addCc(new Cc($cc));
+            }
+        }
+
+        try {
+            // send mail
+            if ($this->disableEmailDelivery === true) {
+                return null;
+            }
+            return parent::send($mail);
+        } catch (Exception $e) {
+            echo 'Caught exception: ',  $e->getMessage(), '\n';
+        }
     }
 }
--- tests/MailerTest.php
+++ tests/MailerTest.php
@@ -8,8 +8,117 @@ use Serato\SendGrid\Mailer;
 
 class MailerTest extends TestCase
 {
-    public function testPlaceHolder()
+    const NOT_DISABLE_EMAIL_DELIVERY = false;
+    const DISABLE_EMAIL_DELIVERY = true;
+    const TEMPLATE_NAME = 'studio-sub-voluntary-cancel';
+    const API_KEY = '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'; // real,not used
+    const FAKE_API_KEY = 'fake-api-key';
+
+
+    /**
+     * Test email options by a valid template name
+     * @group mail
+     */
+    public function testFetchEmailOptionsByTemplateNameWithValidTemplateName()
     {
-        $this->assertTrue(true);
+        $mail = new Mailer(self:: FAKE_API_KEY, self::DISABLE_EMAIL_DELIVERY);
+        $emailOption = $mail->fetchEmailOptionsByTemplateName(self::TEMPLATE_NAME);
+
+        $this->assertNotNull($emailOption);
+    }
+
+    /**
+     * Test email options by an invalid template name
+     * @group mail
+     *
+     * @expectedException \Exception
+     */
+    public function testFetchEmailOptionsByTemplateNameWithInvalidTemplateName()
+    {
+        $templateName = 'invalid-template-name';
+
+        $mail = new Mailer(self:: FAKE_API_KEY, self::DISABLE_EMAIL_DELIVERY);
+        $emailOption = $mail->fetchEmailOptionsByTemplateName($templateName);
+    }
+
+    /**
+     * Test template params are valid with valid params
+     * @group mail
+     */
+    public function testValidateTemplateParamsWithValidParams()
+    {
+        $templateParams = ['subscription_end_date'=>'2017-12-12', 'plan_name'=>'DJ'];
+
+        $mail = new Mailer(self:: FAKE_API_KEY, self::DISABLE_EMAIL_DELIVERY);
+        $mailOptions = $mail->fetchEmailOptionsByTemplateName(self::TEMPLATE_NAME);
+
+        $validTemplateParams = $mail->validateTemplateParams($templateParams, $mailOptions['template_params']);
+
+        $this->assertEquals($validTemplateParams, $templateParams);
+    }
+
+    /**
+     * Test template params are valid with invalid params
+     * @group mail
+     */
+    public function testValidateTemplateParamsWithInvalidParams()
+    {
+        $templateParams = ['invalid_param'=>'2017-12-12', 'plan_name'=>'DJ'];
+
+        $mail = new Mailer(self:: FAKE_API_KEY, self::DISABLE_EMAIL_DELIVERY);
+        $mailOptions = $mail->fetchEmailOptionsByTemplateName(self::TEMPLATE_NAME);
+
+        $validTemplateParams = $mail->validateTemplateParams($templateParams, $mailOptions['template_params']);
+
+        $this->assertEquals($validTemplateParams, ['plan_name'=>'DJ']);
+    }
+
+    /**
+     * Test template language are valid with valid language
+     * @group mail
+     */
+    public function testValidateEmailLanguageWithValidlanguage()
+    {
+        $language = 'en';
+
+        $mail = new Mailer(self:: FAKE_API_KEY, self::DISABLE_EMAIL_DELIVERY);
+        $mailOptions = $mail->fetchEmailOptionsByTemplateName(self::TEMPLATE_NAME);
+
+        $this->assertTrue($mail->validateEmailLanguage($language, $mailOptions['languages']));
+    }
+
+    /**
+     * Test template language are valid with invalid language
+     * @group mail
+     */
+    public function testValidateEmailLanguageWithInvalidlanguage()
+    {
+        $language = 'zh';
+
+        $mail = new Mailer(self:: FAKE_API_KEY, self::DISABLE_EMAIL_DELIVERY);
+        $mailOptions = $mail->fetchEmailOptionsByTemplateName(self::TEMPLATE_NAME);
+
+        $this->assertNotTrue($mail->validateEmailLanguage($language, $mailOptions['languages']));
+    }
+
+    /**
+     * Test that email sent unsuccessfully with disabled email delievery
+     * @group mail
+     */
+    public function testSendMailUnsuccessfully()
+    {
+        $mail = new Mailer(self:: FAKE_API_KEY, self::DISABLE_EMAIL_DELIVERY);
+
+        $sendgridResponse = $mail->sendEmail(
+            self::TEMPLATE_NAME,
+            'Jing Xu',
+            'jing.xu@serato.com',
+            'en',
+            [
+                'subscription_end_date' => '2017-07-12',
+                'plan_name' => 'DJ'
+            ]
+        );
+        $this->assertNull($sendgridResponse);
     }
 }
