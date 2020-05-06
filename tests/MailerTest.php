0567cb4db5644bafe171f5b02a31dd5a723f62a4
MikeS <mike.sabharwal@serato.com>
2020-05-07 05:22:44 +1200

    New email templates
    - Confirm account deactivation email
    - Toolkit welcome
    - User credit card expiring

--- tests/MailerTest.php
+++ tests/MailerTest.php
@@ -25,7 +25,7 @@ class MailerTest extends TestCase
 
         # Check number of email templates
         # Change this number when a new template is added
-        $this->assertEquals(46, count($emailOptions));
+        $this->assertEquals(49, count($emailOptions));
         $templateIds = [];
 
         foreach ($emailOptions as $templateName => $config) {
